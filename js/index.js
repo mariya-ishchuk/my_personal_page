2254487170f682e9406946a30d9da0b4e171c1d8
Mariya Ishchuk <mariya.ishchuk@me.com>
2016-09-06 10:26:47 +0000

    ajax form

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
-var submitF = function(e){
+$(function() {
+    $('#button').click(sendForm);
+});
+
+function sendForm (e) {
     e.preventDefault();
-    console.log(e.target.elements["guest_name"].value);
-};
+    $.ajax({
+        url: "https://formspree.io/mariya.ishchuk@me.com", 
+        method: "POST",
+        data: {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+        },
+        dataType: "json",
+        success: function() {
+            $('form')[0].reset();
+            $('#thanks').html('Thank you for contacting!');
+        }
+    });
+}
