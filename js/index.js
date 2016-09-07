3388cd1b2e983c1456fd448bbfe9f4bae391f0b7
Mariya Ishchuk <mariya.ishchuk@me.com>
2016-09-07 08:25:37 +0000

    form styled

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function sendForm (e) {
         }
     });
 }
-
+// smooth scroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
